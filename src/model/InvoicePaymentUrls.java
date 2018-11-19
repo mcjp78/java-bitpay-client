e25444b1a2d822ec5a6f041b1f024409ec8050c1
Jp78 <mcjp78@users.noreply.github.com>
2018-11-19 15:22:52 -0800

    Update InvoicePaymentUrls.java

--- src/model/InvoicePaymentUrls.java
+++ src/model/InvoicePaymentUrls.java
@@ -1,7 +1,6 @@
 package model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class InvoicePaymentUrls {
